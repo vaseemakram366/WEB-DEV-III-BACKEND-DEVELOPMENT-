dd9505ee2585e9939754b4905662c817c83984d7
Vaseem Akram <vaseemakrama366@gmail.com>
2026-08-13 19:47:34 +0530

    crypto and dns

--- lecture 5/core_modules.js
+++ lecture 5/core_modules.js
@@ -1,8 +1,8 @@
 const os=require('os');
-
 const path = require('path')
-
 const fs = require("fs")
+const crypto = require("crypto")
+const dns = require("dns")
 
 // console.log(os.platform());
 // console.log(os.arch());
@@ -26,25 +26,81 @@ const fs = require("fs")
 // console.log(path.dirname(file));
 
 
-console.log("Deepesh");
-const data = fs.readFileSync("file.txt","utf-8"); // Synchronous
-console.log(data);
-console.log("Yuvraj");
+// console.log("Deepesh");
+// const data = fs.readFileSync("file.txt","utf-8"); // Synchronous
+// console.log(data);
+// console.log("Yuvraj");
+
+// console.log("Aashish");
+// fs.readFile("file.txt","utf-8",(err,data)=>{ // Asynchronous
+//     if(err){
+//         console.log(err);
+        
+//     }else{
+//         console.log(data);
+        
+//     }
+// })
+// console.log("Nitin");
 
 
 
+// fs.writeFile("file.txt","hello students",(err)=>{
+//     if(err){
+//         console.log(err);
+        
+//     }
+// })
 
-console.log("Aashish");
-fs.readFile("file.txt","utf-8",(err,data)=>{ // Asynchronous
+// fs.appendFile("file.txt","\nhello students", (err)=>{
+//     if(err){
+//         console.log(err);
+        
+//     }
+// })
+
+// fs.unlink("file1txt",(err)=>{
+//     if(err){
+//         console.log(err);
+        
+//     }
+// })
+
+// const password="vaseem@123"
+
+// // const hash=crypto.createHash("sha256").update(password).digest("hex");
+// // console.log(hash);
+
+// const salt = crypto.randomBytes(16).toString("hex");
+// // console.log(salt);
+
+
+
+// const hash = crypto.createHmac("sha256",salt).update(password).digest("hex");// uses in compay
+// console.log(hash);
+
+dns.lookup("www.google.com",(err,address,family)=>{
     if(err){
         console.log(err);
         
     }else{
-        console.log(data);
+        console.log(address);
+        console.log(family);
+        
         
     }
 })
-console.log("Nitin");
+
+dns.reverse("8.8.8.8",(err,hostname)=>{
+    if(err){
+        console.log(err);
+        
+    }else{
+        console.log(hostname);
+        
+    }
+})
+
 
 
 
