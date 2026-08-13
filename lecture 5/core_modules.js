dfc14d92e6944e9e17158e728dfb4d0c67c4d82c
Vaseem Akram <vaseemakrama366@gmail.com>
2026-08-13 12:54:28 +0530

    file Path

--- lecture 5/core_modules.js
+++ lecture 5/core_modules.js
@@ -1,11 +1,20 @@
 const os=require('os');
 
-console.log(os.platform());
-console.log(os.arch());
-console.log(os.cpus().length);
-console.log(os.homedir());
-console.log(os.totalmem()/(1024*1024*1024) + " GB");
-console.log(os.freemem()/(1024*1024) + " MB");
+const path = require('path')
+
+// console.log(os.platform());
+// console.log(os.arch());
+// console.log(os.cpus().length);
+// console.log(os.homedir());
+// console.log(os.totalmem()/(1024*1024*1024) + " GB");
+// console.log(os.freemem()/(1024*1024) + " MB");
+
+
+const filePath  = path.resolve("core_modules.js");
+console.log(filePath);
+
+
+
 
 // win32
 // x64
