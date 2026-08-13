f88357823a3e887724d0b0a4c4459d2163bf1858
Vaseem Akram <vaseemakrama366@gmail.com>
2026-08-13 13:25:12 +0530

    synchronous and asynchronous

--- lecture 5/core_modules.js
+++ lecture 5/core_modules.js
@@ -2,6 +2,8 @@ const os=require('os');
 
 const path = require('path')
 
+const fs = require("fs")
+
 // console.log(os.platform());
 // console.log(os.arch());
 // console.log(os.cpus().length);
@@ -10,15 +12,42 @@ const path = require('path')
 // console.log(os.freemem()/(1024*1024) + " MB");
 
 
-const filePath  = path.resolve("core_modules.js");
-console.log(filePath);
+// const filePath  = path.resolve("_dirname","sample","core_modules.js");
+// console.log(filePath);
+// console.log(__dirname);
+
+
+// const filePath = path.join("Core-D", "lecture 5", "core_modules.js");
+// console.log(filePath);
+
+// const file="Core-D\lecture 5\core_modules.js"
+// console.log(path.basename(file));
+// console.log(path.extname(file));
+// console.log(path.dirname(file));
+
+
+console.log("Deepesh");
+const data = fs.readFileSync("file.txt","utf-8"); // Synchronous
+console.log(data);
+console.log("Yuvraj");
 
 
 
 
-// win32
-// x64
-// 12
-// C:\Users\HP
-// 15.637882232666016 GB
-// 4662.328125 MB
+console.log("Aashish");
+fs.readFile("file.txt","utf-8",(err,data)=>{ // Asynchronous
+    if(err){
+        console.log(err);
+        
+    }else{
+        console.log(data);
+        
+    }
+})
+console.log("Nitin");
+
+
+
+
+
+
