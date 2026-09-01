af7469a52b7e0d3fc5fa0237116767ddb6ff605b
Vaseem Akram <vaseemakrama366@gmail.com>
2026-09-01 15:39:43 +0530

    create, update, delete operations

--- lecture 10/server.js
+++ lecture 10/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app=express();
 const PORT= 3000
 
+app.use(express.json()); // it handles thhe json data coming from the client//encode
 const students = [
     {rollNo:1, name:"Ronaldo", section:"core-b"},
     {rollNo:2, name:"Messi", section:"core-b"},
@@ -28,8 +29,40 @@ app.get("/students/:rollNo",(req,res)=>{
 })
 
 
+// Create
+app.post("/students",(req, res)=>{
+    const data = req.body;
+    students.push({rollNo:students.length+1,...data});
+    res.json({success:true, message:"student created successfully", data});
+})
 
+// Update
+app.put("/students/:rollNo", (req, res)=>{
+    const id = req.params.rollNo;
+    const data = req.body;
+    const student=students.find((student)=>student.rollNo===Number(id));
+    if(!student){
+        res.status(404).json({success:false, message:"student not  found"});
+    }
+    student.name=data.name;
+    student.section=data.section;
+    res.json({success:true,student});
+})
 
+// Delete
 
+app.delete("/students/:rollNo",(req,res)=>{
+    const id=req.params.rollNo;
+    const student=students.find((student)=>student.rollNo===Number(id));
+    if(!student){
+        res.status(404).json({success:false, message:"student not  found"});
+    }
+    student.splice(students.indexOf(student),1 );
+    res.json({
+        success:true,
+        message:"student deleted successfully"
+    })
+    
+})
 
 app.listen(PORT, ()=>console.log("Server is running on port 3000"));
